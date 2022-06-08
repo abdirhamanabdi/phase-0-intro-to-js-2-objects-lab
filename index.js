5bf0d6a6d8dda9fb9c5ee4db697622d865ad5b9b
AbdirahmanAbdi <abdirahmanabdi.ma@gmail.com>
2022-06-08 21:17:26 +0300

    your message

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Write your solution in this file!
+const employee = {
+    name : "Abdirahman",
+    streetAddress : " Uhuru Highway",
+
+};
+
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+    return {...employee, [key]: value};
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+
+    
+}
